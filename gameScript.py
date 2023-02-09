efe4ff19fbdf36ff7e28a519eefba4c476a47262
adw9 <adw9@clemson.edu>
2023-02-09 15:43:00 -0500

    fix sys.exit() error

--- gameScript.py
+++ gameScript.py
@@ -13,7 +13,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() 
-            sys.quit()
+            sys.exit()
     
     
     obj.viewUpdate(user)   
