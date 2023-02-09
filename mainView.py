53cab9de3a0221cdd5f576f7fb43e5a17135286a
adw9 <adw9@clemson.edu>
2023-02-08 23:35:56 -0500

    Add working menu popup, placeholder rect

--- mainView.py
+++ mainView.py
@@ -2,8 +2,27 @@ import pygame
 import sys
 
 class View():
-     def __init__(self):
-        pygame.display.set_caption("I have no idea what im doing")
-        SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
-        surface = pygame.display.set_mode(SCREEN_SIZE)
+   SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
+   surface = pygame.display.set_mode(SCREEN_SIZE)
+   #colors
+   screenColor = (50,50,50)
+
+   #placeholder, will be replaced by paddle class
+   gameRect = pygame.Rect(50, 50, 50, 50)
+   rectColor = (255, 0, 0)
+
+
+   def __init__(self):
+      pygame.display.set_caption("I have no idea what im doing")
+      SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
+      surface = pygame.display.set_mode(SCREEN_SIZE)
+
+      #This will take paddle and ball as an arguement soon
+   def viewUpdate(self):
+      
+      self.surface.fill(self.screenColor)
+      pygame.draw.rect(self.surface, self.rectColor, self.gameRect)
+      
+      pygame.display.update()
+
 
