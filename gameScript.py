6083be2bd14cd11eda1378c67fc625276f97ecc4
adw9 <adw9@clemson.edu>
2023-02-08 23:41:16 -0500

    Change Screen Size

--- gameScript.py
+++ gameScript.py
@@ -12,6 +12,9 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() 
-            sys.quit()   
+            sys.quit()
+    
+    
+    obj.viewUpdate()   
 
 
--- mainView.py
+++ mainView.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 
+
 class View():
    SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
    surface = pygame.display.set_mode(SCREEN_SIZE)
@@ -13,8 +14,8 @@ class View():
 
 
    def __init__(self):
-      pygame.display.set_caption("I have no idea what im doing")
-      SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 500,500
+      pygame.display.set_caption("PONG")
+      SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 600,600
       surface = pygame.display.set_mode(SCREEN_SIZE)
 
       #This will take paddle and ball as an arguement soon
