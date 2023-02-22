37043879ee7557f7e8b5d335183261e106229c90
adw9 <adw9@clemson.edu>
2023-02-22 15:10:34 -0500

    add speed var to ball

--- ball.py
+++ ball.py
@@ -4,12 +4,17 @@ import pygame
 
 
 class Ball():
+    #storage vars
     diameter = 0
     color = (0,0,0)
     ballPos = ballX, ballY = 0, 0
+    speedX = 0
+    speedY = 0
 
     def __init__(self):
         self.ballPos = self.ballX, self.ballY = 250, 250
         self.diameter = 10
         self.color = (255,0,0)
+        self.speedX = 2
+        self.speedY = 2
         
